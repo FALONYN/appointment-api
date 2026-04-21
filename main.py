968a3db7b895bed0ab5c54908c53264832c65064
FALONYN <lookfai1339@gmail.com>
2026-04-22 00:02:36 +0700

    Add vercel origin

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ app = FastAPI()
 
 app.add_middleware(
  CORSMiddleware,
- allow_origins=["http://localhost:3000"],
+ allow_origins=["http://localhost:3000",
+                "https://cn334-frontend-2out.vercel.app"],
  allow_credentials=True,
  allow_methods=["*"],
  allow_headers=["*"],
