dd8960e968e679f4be7abd9a7840c57733011896
FALONYN <lookfai1339@gmail.com>
2026-04-22 00:27:51 +0700

    try allow origin *

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
  CORSMiddleware,
- allow_origins=["http://localhost:3000",
-                "https://cn334-frontend-2out.vercel.app"],
+ allow_origins=["*"],
  allow_credentials=True,
  allow_methods=["*"],
  allow_headers=["*"],
